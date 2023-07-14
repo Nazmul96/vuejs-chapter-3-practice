8095f20aed94c0ef07bc7a99993d617f238756ea
Nazmul Hossain <hossainnazmul191@gmail.com>
2023-07-15 01:12:17 +0600

    display data from array

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <script setup>
-    const message = 'Hello I am nazmul hossain. Today i am practiceing Vuejs.'
-    const date = "<strong>10th July, 2023</strong>"
-    const number = 11
+  const images = [
+    {
+        image: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+        title:'Water surrounded by trees and mountains',
+        link:'https://unsplash.com/photos/NRQV-hBF10M'
+    },
 
-    function getDate() {
-      return "11th July, 2024"
-    }
-
-    function evenOrOdd(n) {
-        if (n % 2 == 0) {
-            return "Even"
-        }
-        return "Odd"
-    }
+ ]
 </script>
 
 <template>
-  <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-       <h1 class="text-4xl">{{ message }}</h1>
-       <h2 class="mt-10 text-xl text-gray-700">practiceing date is: {{ getDate() }}</h2>
-       <!-- <h2>{{ number }} is {{evenOrOdd(number)}}</h2> -->
-       <h2 class="mt-10 text-xl text-gray-700">{{ number }} is {{ number % 2 == 0 ? "Even" : "Odd" }}</h2>
-  </div>
+     <section class="container mx-auto p-10">
+        <h1 class="text-4xl mb-10 text-left">Data Project</h1>
+        <section class="main flex space-x-4">
+            <div class="flex space-y-4 flex-col w-1/2">
+                <img :title="images[0].title" :src="images[0].image " :alt="images[0].title">
+                <h2 class="text-xl text-left"></h2>
+                <a target="_blank" :href="images[0].link" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-40 text-center">
+                    Image Source
+                </a>
+            </div>
+        </section>
+    </section>
 </template>
 
 <style scoped>
